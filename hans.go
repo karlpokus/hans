debebaa94181c9e9391081b0f5b8110914b2b12b
Karl Pokus <karl.pokus@gmail.com>
2019-01-08 14:59:27 +0100

    move killer to func

--- hans.go
+++ hans.go
@@ -15,6 +15,26 @@ type Hans struct {
 	Apps []*App
 }
 
+func (hans *Hans) killAppsOnSignal(done chan<- bool) {
+	sigs := make(chan os.Signal, 1) // signals are strings
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	<-sigs // block
+
+	// kill all running apps
+	if len(hans.Apps) > 0 {
+		for _, app := range hans.Apps {
+			if app.Running {
+				hans.Stdout.Printf("Killing %s", app.Name)
+				if err := app.Cmd.Process.Kill(); err != nil {
+					hans.Stderr.Printf("err from cmd.Process.Kill %s", err.Error())
+				}
+			}
+			// TODO: remove app struct from arr
+		}
+	}
+	done <- true
+}
+
 func getPath(p string) string {
 	pwd, _ := os.Getwd()
 	return pwd + p
@@ -38,36 +58,15 @@ func main() {
 	bixa.Cmd.Stdout = bixa
 	rex := createApp("rex", "3")
 	rex.Cmd.Stdout = rex
-
+	
 	hans.Apps = []*App{bixa, rex}
 	hans.Stdout.Print("hans start")
 	for _, app := range hans.Apps {
 		go app.Run(hans)
 	}
 
-	sigs := make(chan os.Signal, 1) // signals are strings
 	done := make(chan bool, 1)
-
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-
-	go func() {
-		<-sigs
-
-		// kill all running apps
-		if len(hans.Apps) > 0 {
-			for _, app := range hans.Apps {
-				if app.Running {
-					hans.Stdout.Printf("Killing %s", app.Name)
-					if err := app.Cmd.Process.Kill(); err != nil {
-						hans.Stderr.Printf("err from cmd.Process.Kill %s", err.Error())
-					}
-				}
-				// TODO: remove app struct from arr
-			}
-		}
-		done <- true
-	}()
-
+	go hans.killAppsOnSignal(done)
 	<-done
-	hans.Stdout.Println("hans exiting")
+	hans.Stdout.Println("hans end")
 }
